7f5c1806fbad37f756f8004244e4fba6bbdbdcde
Krishna-Karkii <krishnakarki1996@gmail.com>
2025-06-08 23:55:52 +0545

    create basic structure for rag pipeline

---
+++ rag_pipeline/generator.py
@@ -0,0 +1,7 @@
+class Model:
+    def __init__(self):
+        pass
+    def _create_system_prompt(self):
+        pass
+    def summarize(self):
+        pass
---
+++ rag_pipeline/retriever.py
@@ -0,0 +1,7 @@
+class Retriever:
+    def __init__(self):
+        pass
+    def _embbed_query(self):
+        pass
+    def retrieve_similar_data(self):
+        pass
